9eb8ff0bd055d9dd9796b6b5c783596a165e25f4
fuckyou1907023 <ha9a1ltt@gmail.com>
2020-04-21 22:43:22 +0700

    upadate lab3

--- T1907M-Csharp/Lab2/Run.cs
+++ T1907M-Csharp/Lab2/Run.cs
@@ -7,61 +7,40 @@ namespace buoi2.Lab2
         public static void Main(string[] args)
         {
             Product product = new Product();
-            int chose = 0;
-            int id;
-            string name;
-            double price;
-            int qty;
-            string image;
-            string desc;
-            do
+                int chose = 0;
+                do
                 {
                     Console.WriteLine("1. Addproduct");
                     Console.WriteLine("2. GetInfo");
                     Console.WriteLine("3. CheckStock");
-                    Console.WriteLine("4. Add gallery");
-                    Console.WriteLine("5. Remove gallery");
+                    Console.WriteLine("4. Add IMG");
+                    Console.WriteLine("5. Remove IMG");
                     Console.WriteLine("6. Exit");
                     Console.WriteLine("Chose: ");
                     chose = Convert.ToInt32(Console.ReadLine());
                     switch (chose)
                     {
                         case 1:
-                            Console.WriteLine("Enter id: ");
-                            id = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("Enter name: ");
-                            name = Convert.ToString(Console.ReadLine());
-                            Console.WriteLine("Enter price: ");
-                            price = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine("Enter qty: ");
-                            qty = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("Enter image: ");
-                            image = Convert.ToString(Console.ReadLine());
-                            Console.WriteLine("Enter desc: ");
-                            desc = Convert.ToString(Console.ReadLine());
-                         break;
+                            product.addProduct(product);
+                            break;
                         case 2:
                             product.showInfor();
                             break;
                         case 3:
-                           product.stock();
+                            product.stock();
+                            
                             break;
                         case 4:
                             product.addGallery();
                             break;
-                        case 5: 
+                        case 5:
                             product.removeGallery();
                             break;
-                        case 6: 
+                        case 6:
                             Environment.Exit(0);
                             break;
-                        default:
-                            Console.WriteLine("Wrong choice please choice 1-6!");
-                            break;
                     }
-                }
-
-                while (chose != 5) ;
+                } while (chose != 5);
+            }
         }
     }
-}
--- T1907M-Csharp/Lab3/Fashion.cs
+++ T1907M-Csharp/Lab3/Fashion.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using buoi2.Lab2;
 using buoi2.SSS3SSSS;
@@ -13,21 +14,59 @@ namespace buoi2.Lab3
         public Fashion()
         {
         }
-
+ 
         public Fashion(string color, string size)
         {
             this.color = color;
             this.size = size;
         }
-
         public Fashion(int id, string name, int price, int qty, string img, string desc, List<string> gallery, string color, string size) : base(id, name, price, qty, img, desc, gallery)
         {
             this.color = color;
             this.size = size;
         }
-
         public void descColor()
         {
+            
+        }
+        public void ShowFashion()
+        {
+            Console.WriteLine("Fashion List:");
+            foreach (Fashion f in productList)
+            {
+                Console.WriteLine("ID:" + id + " - Name: " + name + " - Price: " + price + " - Qty: " + qty +
+                                  " - Desc: " + desc + " - Color: " + color + " - Size " + size);
+            }
+        }
+
+        public void checkFashion()
+        {
+            string name;
+            Console.WriteLine("San pham can tim: ");
+            name = Convert.ToString(Console.ReadLine());
+            foreach (Fashion f in productList)
+            {
+                if (name.Equals(f.name))
+                {
+                    if (f.qty > 0)
+                    {
+                        Console.WriteLine("Con hang");
+                        string color;
+                        Console.WriteLine("Mau can tim: ");
+                        color = Convert.ToString(Console.ReadLine());
+                        if (color.Equals(f.color))
+                        {
+                            Console.WriteLine("ID:" + id + " - Name: " + name + " - Price: " + price + " - Qty: " +
+                                              qty +
+                                              " - Desc: " + desc + " - Color: " + color + " - Size " + size);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Het hang");
+                    }
+                }
+            }
         }
     }
 }
